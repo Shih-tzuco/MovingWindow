1b92e6156a8e308d3135f07f1fc8e37855017355
Anna <Anna@ANNA-VAIO>
2018-06-01 11:18:20 +0300

    Add project files.

---
+++ MovingWindow/Window.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MovingWindow
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.Press_Down);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Window";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Moving Window";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Key_Pressed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ MovingWindow/Window.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MovingWindow
+{
+    public partial class Window : Form
+    { 
+        private string button_Direction;
+        int width = Screen.PrimaryScreen.Bounds.Width;
+        int height = Screen.PrimaryScreen.Bounds.Height;
+        int step = 6;
+
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void Key_Code(Keys key)
+        {
+            timer.Start();
+            switch (key)
+            {
+                case Keys.Down:
+                    button_Direction = "Down";
+                    break;
+                case Keys.Up:
+                    button_Direction = "Up";
+                    break;
+                case Keys.Right:
+                    button_Direction = "Right";
+                    break;
+                case Keys.Left:
+                    button_Direction = "Left";
+                    break;
+                case Keys.Enter:
+                    timer.Stop();
+                    CenterToScreen();
+                    break;
+            }
+        }
+
+        private void Press_Down(object sender, EventArgs e)
+        {
+            if (button_Direction == "Down")
+            {
+                if (Location.Y + Height != height)
+                {
+                    if (Location.Y + step + Height <= height)
+                    {
+                        Location = new Point(Location.X, Location.Y + step);
+                    }
+                    else
+                    {
+                        Location = new Point(Location.X, height - Height);
+                    }
+                }
+                else
+                {
+                    Key_Code(Keys.Up);
+                }
+            }
+            else if (button_Direction == "Up")
+            {
+                if (Location.Y != 0)
+                {
+                    if (Location.Y - step >= 0)
+                    {
+                        Location = new Point(Location.X, Location.Y - step);
+                    }
+                    else
+                    {
+                        Location = new Point(Location.X, 0);
+                    }
+                }
+                else
+                {
+                    Key_Code(Keys.Down);
+                }
+            }
+            else if (button_Direction == "Right")
+            {
+                if (Location.X + Width != width)
+                {
+                    if (Location.X + step + Width <= width)
+                    {
+                        Location = new Point(Location.X + step, Location.Y);
+                    }
+                    else
+                    {
+                        Location = new Point(width - Width, Location.Y);
+                    }
+                }
+                else
+                {
+                    Key_Code(Keys.Left);
+                }
+            }
+            else if (button_Direction == "Left")
+            {
+                if (Location.X != 0)
+                {
+                    if (Location.X - step >= 0)
+                    {
+                        Location = new Point(Location.X - step, Location.Y);
+                    }
+                    else
+                    {
+                        Location = new Point(0, Location.Y);
+                    }
+                }
+                else
+                {
+                    Key_Code(Keys.Right);
+                }
+            }
+        }
+ 
+        private void Key_Pressed(object sender, KeyEventArgs e)
+        {
+            Key_Code(e.KeyCode);
+        }
+    }
+}
